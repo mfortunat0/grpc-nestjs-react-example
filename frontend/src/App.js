4387c71b6465fe0f8570106cbf6f325000dd6d76
matheus <matheus.xmaz10@gmail.com>
2021-08-17 22:38:14 -0300

    working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useRef } from "react";
 
 function App() {
+  const inputRef = useRef();
+  const request = async (e) => {
+    e.preventDefault();
+    console.log(
+      await (
+        await axios.get(
+          `http://localhost:3001?number=${inputRef.current.value}`
+        )
+      ).data
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={(e) => request(e)}>
+      <input required type="number" ref={inputRef} />
+      <input type="submit" value="Send" />
+    </form>
   );
 }
 
